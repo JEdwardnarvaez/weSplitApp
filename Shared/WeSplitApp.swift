ffc2e12a2beb756f2971819f58d3e793b90e580a
John Edward <jnarvaez@ikatechsolutions.com>
2021-09-14 20:15:41 -0500

    stable working project

---
+++ Shared/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  Shared
+//
+//  Created by John Edward Narvaez Londoño on 13/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
